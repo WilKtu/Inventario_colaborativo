adbfc3604a9c981be9938582b52341b50afeb554
Anderson-Oloroso <henrikog4@gmail.com>
2026-03-09 08:47:11 -0600

    :sparkles: Feat: Menu

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+from inventario import *
+def separador():
+    print("========================================")
+
+def menu():
+    mensaje = """    1. Agregar Producto
+    2. Listar Productos
+    3. Salir
+"""
+    separador()
+    print("      SISTEMA DE INVENTARIO")
+    separador()
+    print(mensaje)
+    
+
+def main():
+    while True:
+        menu()
+        try: 
+            op = int(input("Ingrese una opcion: "))
+            if op == 1:
+                separador()
+                print("AGREGAR PRODUCTO")
+                separador()
+                agregar_producto()
+            elif op == 2:
+                separador()
+                print("LISTAR PRODUCTOS")
+                separador()
+                agregar_producto()
+            elif op == 3: 
+                separador()
+                print("SALIENDO DEL PROGRAMA ...")
+                sys.exit(0)
+            else:
+                separador()
+                print("Opción inválida")
+        except ValueError as v:
+            print("Error: ", v)
+            
+if __name__ == "__main__": 
+    main() 
